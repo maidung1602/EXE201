75eafcbf16d8858d5b733495bd431b5e55c8c8b2
maidung1602 <maidung1602@gmail.com>
2024-09-28 21:18:57 +0700

    fix fefe

--- client/src/components/shop/home/Story.js
+++ client/src/components/shop/home/Story.js
@@ -11,33 +11,6 @@ const StoryComponent = () => {
 					<div
 						className="hero-section"
 						style={{
-							backgroundImage: `url('https://madebymaries.com/wp-content/uploads/2021/10/feel-maries.png')`,
-							backgroundSize: "cover",
-							backgroundPosition: "center",
-							color: "#fff",
-							padding: "100px 0",
-							textAlign: "center",
-							height: "400px",
-							display: "flex",
-							alignItems: "center",
-							justifyContent: "center",
-						}}
-					>
-						<Container>
-							<Row>
-								<Col>
-									<h1>Chuyện của Maries</h1>
-									<p>Chuyện của Đan Tâm Việt</p>
-								</Col>
-							</Row>
-						</Container>
-					</div>
-				</Carousel.Item>
-
-				<Carousel.Item>
-					<div
-						className="hero-section"
-						style={{
 							backgroundImage: `url('https://cdnphoto.dantri.com.vn/9J6rE2rOLqd246m-_NAsxrr71qQ=/zoom/1200_630/2021/07/14/ho-thi-suong-lan-crop-1626253035534.jpeg')`,
 							backgroundSize: "cover",
 							backgroundPosition: "center",
@@ -97,17 +70,12 @@ const StoryComponent = () => {
 				<Container>
 					<Row className="mb-5">
 						<Col md={8}>
-							<h2>CÂU CHUYỆN CỎ BÀNG XỨ HUẾ</h2>
+							<h2>Ý NGHĨA THƯƠNG HIỆU</h2>
 							<p>
-								Phò Trạch hay còn được người làng gọi bằng tên thân thương là
-								Phò Trạch Đệm, là một trong nhiều ngôi làng yên bình nép mình
-								bên dòng sông Ô Lâu đổ ra phá Tam Giang thuộc tỉnh Thừa Thiên
-								Huế. Phò Trạch được hình thành cách đây gần 500 năm từ lúc các
-								vị chúa Nguyễn hành trình về phương Nam, là địa phương thuộc một
-								trong những khu vực có địa hình thấp trũng nên hầu như năm nào
-								cũng bị ảnh hưởng lũ lụt. Thế nhưng người dân nơi đây lại được
-								thừa hưởng từ cha ông nghề thủ công đan lát truyền thống và độc
-								đáo từ cây Cỏ Bàng.
+							<strong>Đan Tâm Việt</strong> là nơi gửi gắm tinh hoa thủ công truyền thống Việt Nam, nơi mà mỗi sản phẩm mây tre đan không chỉ đơn thuần là vật dụng, mà là những câu chuyện được kể bằng đôi bàn tay tài hoa và trái tim chân thành. Từ những sợi mây tre mỏng manh, qua bàn tay khéo léo của các nghệ nhân, họ dệt nên những giá trị vượt thời gian, mang hơi thở của thiên nhiên và tinh hoa văn hóa Việt.
+							</p>
+							<p>
+							Mỗi sản phẩm của <strong>Đan Tâm Việt</strong> là một biểu tượng của sự kết nối giữa con người và đất trời, là tình yêu, sự tận tụy mà chúng tôi dành trọn trong từng đường đan, từng chi tiết. Mỗi sản phẩm là một lời nhắc nhở về sự giản dị, tinh tế và sâu lắng, là những giá trị không bao giờ phai nhạt theo thời gian.
 							</p>
 						</Col>
 						<Col md={4}>
@@ -125,161 +93,31 @@ const StoryComponent = () => {
 							/>
 						</Col>
 						<Col md={8}>
-							<h2>Các họa sĩ của Maries</h2>
+							<h2>SỨ MỆNH</h2>
 							<p>
-								Maries là thương hiệu hàng thủ công mỹ nghệ của Việt Nam, được
-								thành lập chính thức tại Thành phố Huế vào tháng 4 năm 2021 đúng
-								vào thời điểm cả nước bị ảnh hưởng nặng nề bởi Covid-19. Maries
-								sản xuất và quảng bá các sản phẩm thời trang handmade, đáp ứng
-								xu hướng hiện nay, dựa trên các kỹ năng thủ công truyền thống,
-								kết nối những ngành nghề truyền thống có tuổi đời hàng trăm năm
-								với nhau – là nơi có những nghệ nhân, nông dân là phụ nữ đã gắn
-								bó với nghề qua nhiều thế hệ tại Huế. Sản phẩm chủ đạo gồm Túi
-								xách, Nón, Mũ, Ví, được làm với một quy trình khép kín từ Cây Cỏ
-								Bàng.
+							<strong>Đan Tâm Việt</strong> khao khát kết nối người tiêu dùng với vẻ đẹp của sự mộc mạc, thanh thoát từ những sợi mây tre và mang lại cảm giác bình yên và sự gắn kết với văn hóa quê hương, qua từng sản phẩm, lan tỏa niềm tự hào về một nền thủ công truyền thống đầy giá trị.
+							</p>
+							<p>
+							Bằng tất cả sự tận tụy và đam mê, <strong>Đan Tâm Việt</strong> tự hào lan tỏa niềm tự hào dân tộc qua từng tác phẩm, như một lời nhắc nhở về vẻ đẹp trường tồn của nghề thủ công truyền thống, vẫn luôn sống động và rực rỡ trong đời sống hiện đại.
 							</p>
 						</Col>
 					</Row>
 					<Row className="mb-5">
 						<Col md={6}>
+						<h2>CAM KẾT</h2>
 							<p>
-								Khác với cây Cói ở miền Bắc, cây Cỏ Bàng xứ Huế có từng đốt nhỏ,
-								rỗng ruột và không có phần xốp bên trong. Khác với cây Cỏ Bàng
-								miền Tây bởi thân cây Cỏ Bàng Huế nhỏ, đường kính chỉ tầm 0,4cm
-								trở lại...
+							Chúng tôi cam kết sử dụng nguyên liệu từ thiên nhiên, thân thiện với môi trường và tuân thủ các tiêu chuẩn sản xuất bền vững, nhằm bảo vệ môi trường sống và góp phần xây dựng một tương lai xanh hơn. <strong>Đan Tâm Việt</strong> không ngừng nỗ lực gìn giữ và phát huy giá trị của nghề thủ công truyền thống, đồng thời mang lại cho khách hàng những trải nghiệm gần gũi, ấm áp và gắn kết với văn hóa Việt.
 							</p>
 							<p>
-								Tuy mang trong mình một làng nghề thủ công có giá trị về lịch
-								sử, văn hóa và truyền thống nhưng sự phát triển của làng nghề
-								đan lát Cỏ Bàng xứ Huế cũng đang đối mặt với không ít thách
-								thức...
+							Chúng tôi nỗ lực gìn giữ và phát huy giá trị của nghề thủ công truyền thống thông qua việc hợp tác với các nghệ nhân địa phương. Điều này không chỉ tạo ra sản phẩm độc đáo mà còn giúp duy trì văn hóa và truyền thống của quê hương.
+
 							</p>
 						</Col>
 						<Col md={6}>
 							<Image
-								src="https://dantra.vn/uploads/san-pham/thoi-trang/tui-di-bien/tui-coi-thoi-trang-7.jpg"
+								src="https://bizweb.dktcdn.net/thumb/grande/100/401/675/files/mayhousedecor-ve-lang-dan-lat-bao-la3.jpg?v=1612687601663"
 								fluid
 							/>
-						</Col>
-					</Row>
-					<Row>
-						<Col md={6}>
-							<Image
-								src="https://dantra.vn/uploads/san-pham/thoi-trang/tui-di-bien/tui-coi-thoi-trang-6.jpg"
-								fluid
-							/>
-						</Col>
-						<Col md={6}>
-							<p>
-								Trong khi nhu cầu của xã hội hiện đại ngày càng cao mà sản phẩm
-								ở địa phương lại chưa đáp ứng được ước muốn của thị trường. Cộng
-								đồng đứng trước nguy cơ mất đi nghề truyền thống...
-							</p>
-							<p>
-								Một sứ mệnh đã bắt đầu như một nhóm trong chúng tôi, ước muốn
-								gây dựng sản phẩm hàng thủ công có thương hiệu, mang tính độc
-								đáo và có giá trị thẩm mỹ cao với sứ mệnh nuôi dưỡng và nâng cao
-								giá trị của nghề truyền thống Huế... Trong khi nhu cầu của xã
-								hội hiện đại ngày càng cao mà sản phẩm ở địa phương lại chưa đáp
-								ứng được ước muốn của thị trường. Cộng đồng đứng trước nguy cơ
-								mất đi nghề truyền thống. Nhưng những người phụ nữ nơi đây vẫn
-								luôn muốn giữ gìn và phát triển nghề đệm bàng truyền thống Phò
-								Trạch. Một sứ mệnh đã bắt đầu nhen nhóm trong chúng tôi, ước
-								muốn gầy dựng sản phẩm hàng thủ công có thương hiệu, mang tính
-								độc đáo và có giá trị thẩm mỹ cao với sứ mênh nuôi dưỡng và nâng
-								cao giá trị của nghề truyền thống Huế. Đồng thời cùng cộng đồng
-								xây dựng sinh kế bền vững hơn, vừa duy trì được công việc truyền
-								thống ngay tại quê hương mình, vừa nâng cao thu nhập.
-							</p>
-						</Col>
-					</Row>
-					<Row className="mb-5 mt-5">
-						<Col>
-							<div
-								className="image-section"
-								style={{
-									position: "relative",
-									textAlign: "center",
-									color: "white",
-								}}
-							>
-								<Image
-									src="https://heritagevietnamairlines.cdn.vccloud.vn/wp-content/uploads/2023/07/NTT51371-scaled.jpg"
-									fluid
-									style={{ width: "100%", borderRadius: "8px" }}
-								/>
-								<div
-									className="centered-text"
-									style={{
-										position: "absolute",
-										top: "50%",
-										left: "50%",
-										transform: "translate(-50%, -50%)",
-										backgroundColor: "rgba(0, 0, 0, 0.5)",
-										padding: "10px 20px",
-										borderRadius: "5px",
-									}}
-								>
-									<h2>Các họa sĩ của Maries</h2>
-								</div>
-							</div>
-							<p style={{ marginTop: "20px" }}>
-								Maries là thương hiệu hàng thủ công mỹ nghệ của Việt Nam, được
-								thành lập chính thức tại Thành phố Huế vào tháng 4 năm 2021 đúng
-								vào thời điểm cả nước bị ảnh hưởng nặng nề bởi Covid-19.
-							</p>
-							<p>
-								Maries sản xuất và quảng bá các sản phẩm thời trang handmade,
-								đáp ứng xu hướng hiện nay, dựa trên các kỹ năng thủ công truyền
-								thống, kết nối những ngành nghề truyền thống có tuổi đời hàng
-								trăm năm với nhau – là nơi có những nghệ nhân, nông dân là phụ
-								nữ đã gắn bó với nghề qua nhiều thế hệ tại Huế. Sản phẩm chủ đạo
-								gồm Túi xách, Nón, Mũ, Ví, được làm với một quy trình khép kín
-								từ Cây Cỏ Bàng.
-							</p>
-						</Col>
-					</Row>
-					<Row className="mb-5 mt-5">
-						<Col md={4}>
-							<Image
-								src="https://file.baothuathienhue.vn/data2/image/fckeditor/upload/2019/20190716/images/dot.jpg"
-								thumbnail
-							/>
-						</Col>
-						<Col md={4}>
-							<Image
-								src="https://file.baothuathienhue.vn/data2/image/fckeditor/upload/2019/20190716/images/dot.jpg"
-								thumbnail
-							/>
-						</Col>
-						<Col md={4}>
-							<Image
-								src="https://file.baothuathienhue.vn/data2/image/fckeditor/upload/2019/20190716/images/dot.jpg"
-								thumbnail
-							/>
-						</Col>
-					</Row>
-					<Row className="mb-5 mt-5">
-						<Col>
-							<p style={{ marginTop: "20px" }}>
-								Cảm ơn cuộc đời vì tôi được sinh ra là một người nữ, được trao
-								cho đặc quyền thấu hiểu, yêu thương và đồng cảm với những người
-								phụ nữ khác, vừa phải làm trọn vẹn vai trò của người mẹ, người
-								vợ, người bà, vừa khao khát lao động kiếm tiền chân chính. Với
-								Maries, tôi muốn nâng cao vai trò và tiếng nói của người phụ nữ,
-								khơi dậy niềm tự hào của họ về cộng đồng, làng quê nơi họ sinh
-								sống, là từng bước hiện thực hóa giấc mơ về những “Ngôi nhà hạnh
-								phúc – Ngôi làng thịnh vượng”.
-							</p>
-							<p>
-								Maries sản xuất và quảng bá các sản phẩm thời trang handmade,
-								đáp ứng xu hướng hiện nay, dựa trên các kỹ năng thủ công truyền
-								thống, kết nối những ngành nghề truyền thống có tuổi đời hàng
-								trăm năm với nhau – là nơi có những nghệ nhân, nông dân là phụ
-								nữ đã gắn bó với nghề qua nhiều thế hệ tại Huế. Sản phẩm chủ đạo
-								gồm Túi xách, Nón, Mũ, Ví, được làm với một quy trình khép kín
-								từ Cây Cỏ Bàng.
-							</p>
 						</Col>
 					</Row>
 				</Container>
--- client/src/components/shop/partials/Footer.js
+++ client/src/components/shop/partials/Footer.js
@@ -1,17 +1,81 @@
 import React, { Fragment } from "react";
 import moment from "moment";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faLocationPin,
+	faPhone,
+	faEnvelope
+} from "@fortawesome/free-solid-svg-icons";
+import {
+	faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = (props) => {
-  return (
-    <Fragment>
-      <footer
-        style={{ background: "#303031", color: "#87898A" }}
-        className="z-10 py-6 px-4 md:px-12 text-center"
-      >
-        Develop & Design by Đan Tâm Việt © Copyright {moment().format("YYYY")}
-      </footer>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<footer style={{ background: "#143F3E", color: "#ffffff" }}>
+				<div className="container-fluid z-10 py-6 px-4 md:px-12">
+					<div className="grid grid-cols-12 gap-8">
+						<div className="col-span-4 flex justify-center md:justify-end px-24">
+							<img src="/Logo_light_font.png" width="140" className="py-6" alt="Logo" /> {/* Reduced width */}
+						</div>
+						<div className="col-span-3 flex flex-col py-6 ">
+							<h5>Địa chỉ</h5>
+							<div className="flex space-x-4 py-3">
+								<FontAwesomeIcon icon={faLocationPin} style={{ fontSize: '1.5rem' }} className="text-gray-400 hover:text-white" />
+								<a
+									href="https://maps.app.goo.gl/xwzuJz3jK2np5DMw6"
+									target="_blank"
+									rel="noopener noreferrer"
+									className="text-white hover:text-white ml-4" style={{ fontSize: '20px', textDecoration: 'none' }}>
+									Khu Công nghệ cao Hòa Lạc – Km29 Đại lộ Thăng Long, H. Thạch Thất, TP. Hà Nội
+								</a>
+							</div>
+						</div>
+						<div className="col-span-5 flex flex-col py-6 px-12">
+							<h5>Liên hệ</h5>
+							<div className="flex space-x-4 py-2">
+								<FontAwesomeIcon icon={faPhone} style={{ fontSize: '1.5rem' }} className="text-gray-400 hover:text-white" />
+								<a
+									href="tel:0834235369"
+									target="_blank"
+									rel="noopener noreferrer"
+									className="text-white hover:text-white ml-4" style={{ fontSize: '20px', textDecoration: 'none' }}>
+									0834 235 369
+								</a>
+							</div>
+							<div className="flex space-x-4 py-2">
+								<FontAwesomeIcon icon={faEnvelope} style={{ fontSize: '1.5rem' }} className="text-gray-400 hover:text-white" />
+								<a
+									href="mailto:ducnmhe173177@fpt.edu.vn"
+									target="_blank"
+									rel="noopener noreferrer"
+									className="text-white hover:text-white ml-4" style={{ fontSize: '20px', textDecoration: 'none' }}>
+									ducnmhe173177@fpt.edu.vn
+								</a>
+							</div>
+							<div className="flex space-x-4 py-2">
+								<FontAwesomeIcon icon={faFacebook} style={{ fontSize: '1.5rem' }} className="text-gray-400 hover:text-white" />
+								<a
+									href="https://www.facebook.com/dantamviet/?notif_id=1727315130535398&notif_t=follower_invite&ref=notif"
+									target="_blank"
+									rel="noopener noreferrer"
+									className="text-white hover:text-white ml-4" style={{ fontSize: '20px', textDecoration: 'none' }}>
+									Đan Tâm Việt
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div
+					style={{ background: "#152c2c" }}
+					className="z-10 py-6 px-4 md:px-12 text-center"
+				>
+					Develop & Design By Đan Tâm Việt © Copyright {moment().format("YYYY")}
+				</div>
+			</footer>
+		</Fragment>
+	);
 };
 
 export default Footer;
